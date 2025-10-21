d4f29cd763e11900d0cfe6d8035551ae5bfd83c8
theabm <72303015+theabm@users.noreply.github.com>
2025-10-21 14:16:39 +0200

    update flake - self contained

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,11 +1,9 @@
 {
   inputs.nixpkgs.url = "github:nixOS/nixpkgs/nixos-unstable";
+
   inputs.systems.url = "github:nix-systems/default";
   inputs.parts.url = "github:hercules-ci/flake-parts";
   inputs.parts.inputs.nixpkgs-lib.follows = "nixpkgs";
-
-  inputs.nixpkgs-dask.url = "github:nixOS/nixpkgs/a42717b875f8d9da831ed7a8ceab1dc986ce518b"; # 2024.6.0
-  inputs.nixpkgs-ray.url = "github:nixOS/nixpkgs/4f6c942f7a68ee2c05832531d42370c6268e7ff1"; # 2.47.1
 
   outputs = inputs: inputs.parts.lib.mkFlake { inherit inputs; } {
     systems = import inputs.systems;
@@ -13,35 +11,100 @@
     perSystem = { lib, pkgs, system, ... }:
       let
         python = pkgs.${pythonVersion};
-        pythonVersion = "python312";
-        pythonPkgFrom = nixpkgs: name: {
-          ${name} = nixpkgs.legacyPackages.${system}."${pythonVersion}Packages".${name};
-        };
+        pythonVersion = "python313";
       in
-      {
+        {
         _module.args.pkgs = import inputs.nixpkgs {
           inherit system;
           overlays = [
             (final: prev: {
               ${pythonVersion} = prev.${pythonVersion}.override {
                 packageOverrides = pyFinal: pyPrev:
-                  pythonPkgFrom inputs.nixpkgs-dask "dask"
-                  //
-                  pythonPkgFrom inputs.nixpkgs-ray "ray";
+                  { 
+                    ray = pyPrev.ray.overridePythonAttrs ( old: rec {
+                      version = "2.47.1";
+                      src = let 
+                        platforms = {
+                          aarch64-darwin = "macosx_12_0_arm64";
+                          aarch64-linux = "manylinux2014_aarch64";
+                          x86_64-linux = "manylinux2014_x86_64";
+                        };
+                        pyShortVersion = "cp${builtins.replaceStrings [ "." ] [ "" ] python.pythonVersion}";
+                        hashes = {
+                          x86_64-linux = "sha256-JSpHHor7kYsQXNv/tMvrsBQ7qtdaBsj/zeJ6wxdXnMs=";
+                          aarch64-linux = "";
+                          aarch64-darwin = "";
+                        };
+                      in 
+
+                        pyPrev.fetchPypi {
+                          inherit (old) pname format;
+                          inherit version;
+                          dist = pyShortVersion;
+                          python = pyShortVersion;
+                          abi = pyShortVersion;
+                          platform = platforms.${prev.stdenv.hostPlatform.system} or { };
+                          sha256 = hashes.${prev.stdenv.hostPlatform.system} or { };
+                        };
+                    });
+
+                    dask = pyPrev.dask.overridePythonAttrs ( old: rec {
+                      version = "2024.6.0";
+                      src = prev.fetchFromGitHub {
+                        owner = "dask";
+                        repo = "dask";
+                        tag = version;
+                        hash = "sha256-HtWxVWMk0G2OeBnZKLF5tuOohPbg20Ufl+VH/MX8vK0=";
+                      };
+                      postPatch = ''
+                        # versioneer hack to set version of GitHub package
+                        echo "def get_versions(): return {'dirty': False, 'error': None, 'full-revisionid': None, 'version': '${version}'}" > dask/_version.py
+                      
+                        substituteInPlace setup.py \
+                        --replace-fail "import versioneer" "" \
+                        --replace-fail "version=versioneer.get_version()," "version='${version}'," \
+                        --replace-fail "cmdclass=versioneer.get_cmdclass()," ""
+                      
+                        substituteInPlace pyproject.toml \
+                        --replace-fail ', "versioneer[toml]==0.29"' ""
+                        '';
+                      dependencies = old.dependencies ++ [pyPrev.numpy pyPrev.pandas];
+                      doCheck=false;
+                    });
+
+                    doreisa = pyPrev.buildPythonPackage rec{
+                      pname = "doreisa";
+                      version = "0.3.4";
+                      pyproject = true;
+                      src = prev.fetchFromGitHub {
+                        owner = "deisa-project";
+                        repo = "doreisa";
+                        tag = "v${version}";
+                        hash = "sha256-+W62cinBU//RZZcSuuvL6TCjdRZtGd0Hizy7GeUXH0w=";
+                      };
+                      build-system = [pyPrev.hatchling];
+                      pythonRelaxDeps = ["numpy"];
+                      dependencies = with pyFinal; [numpy ray dask ] ++ pyFinal.dask.optional-dependencies.dataframe;
+                      doCheck=false;
+                    };
+                  };
               };
             })
           ];
         };
 
         devShells.default = pkgs.mkShell {
+          USE_NIX=1;
           packages = with pkgs; [
             mpi
             libyaml
             gcc
             imagemagick
+            uv
             (python.withPackages (_: with _; [
-              (toPythonModule ray)
-              (toPythonModule dask)
+              ray
+              dask
+              doreisa
 
               cloudpickle
               filelock
@@ -70,12 +133,12 @@
             export OMP_NUM_THREADS=1
             export MKL_NUM_THREADS=1
             export OPENBLAS_NUM_THREADS=1
-          '';
+            '';
         };
 
         formatter = pkgs.writeShellScriptBin "formatter" ''
           ${lib.getExe pkgs.nixpkgs-fmt} .
-        '';
+          '';
       };
   };
 }
